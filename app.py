abc928152f6c8b4c28f6cf892de2010f074d2040
Ezzaldien Ezzat <83853780+ezzaldien@users.noreply.github.com>
2024-05-05 11:14:53 +0000

    added Html & CSS

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def helloWorld():
-  return "Hello jovian!"
+def hello_zoii():
+  return render_template('home.html')
 
 
 if __name__ == "__main__":
