84d3de0ca6ea6b13790daeedbd3c05015f0bc0fc
Ezzaldien Ezzat <83853780+ezzaldien@users.noreply.github.com>
2024-05-07 15:58:12 +0000

    added dinamic data

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 'Rs. 12,00,000'
+  }
+]
 
 @app.route("/")
 def hello_zoii():
-  return render_template('home.html')
+  return render_template('home.html' , jobs=JOBS)
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
